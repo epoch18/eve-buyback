c31e743c24aec1b434076494e0bd191fe3f590f3
Mike Sims <msims04@gmail.com>
2016-02-20 18:19:06 -0500

    add missing test for the update items job

---
+++ tests/app/Console/Commands/UpdateItemsCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UpdateItemsCommandTest extends TestCase
+{
+	use DatabaseMigrations;
+
+	/**
+	 * @var \Illuminate\Bus\Dispatcher
+	 */
+	private $dispatcher;
+
+	/**
+	 * @var \App\Jobs\UpdateItemsJob
+	 */
+	private $job;
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->dispatcher = Mockery::mock(\Illuminate\Bus\Dispatcher::class);
+		$this->job        = Mockery::mock(\App\Jobs\UpdateItemsJob::class);
+	}
+
+	public function testHandle()
+	{
+		$this->dispatcher->shouldReceive('dispatchNow')->once()->with($this->job);
+
+		$command = new \App\Console\Commands\UpdateItemsCommand($this->dispatcher, $this->job);
+		$command->handle();
+	}
+}
