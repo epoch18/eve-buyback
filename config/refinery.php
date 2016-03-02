60e11406497e302d918e8968ccc1db88f8a6e834
Mike Sims <msims04@gmail.com>
2016-03-02 17:37:05 -0500

    move settings to .env file instead of database

---
+++ config/refinery.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+	'beancounter'   => env('EVE_PROCESSING_BEANCOUNTER'            , 4   ),
+	'station_tax'   => env('EVE_PROCESSING_STATION_TAX'            , 1.00),
+	'station_yield' => env('EVE_PROCESSING_STATION_YIELD'          , 0.52),
+	'reprocessing'  => env('EVE_PROCESSING_REPROCESSING'           , 5   ),
+	'efficiency'    => env('EVE_PROCESSING_REPROCESSING_EFFICIENCY', 5   ),
+	'ice'           => env('EVE_PROCESSING_ICE'                    , 5   ),
+	'scrapmetal'    => env('EVE_PROCESSING_SCRAPMETAL'             , 5   ),
+	'arkonor'       => env('EVE_PROCESSING_ARKONOR'                , 5   ),
+	'bistot'        => env('EVE_PROCESSING_BISTOT'                 , 5   ),
+	'crokite'       => env('EVE_PROCESSING_CROKITE'                , 5   ),
+	'dark_ochre'    => env('EVE_PROCESSING_DARK_OCHRE'             , 5   ),
+	'gneiss'        => env('EVE_PROCESSING_GNEISS'                 , 5   ),
+	'hedbergite'    => env('EVE_PROCESSING_HEDBERGITE'             , 5   ),
+	'hemorphite'    => env('EVE_PROCESSING_HEMORPHITE'             , 5   ),
+	'jaspet'        => env('EVE_PROCESSING_JASPET'                 , 5   ),
+	'kernite'       => env('EVE_PROCESSING_KERNITE'                , 5   ),
+	'mercoxit'      => env('EVE_PROCESSING_MERCOXIT'               , 5   ),
+	'omber'         => env('EVE_PROCESSING_OMBER'                  , 5   ),
+	'plagioclase'   => env('EVE_PROCESSING_PLAGIOCLASE'            , 5   ),
+	'pyroxeres'     => env('EVE_PROCESSING_PYROXERES'              , 5   ),
+	'scordite'      => env('EVE_PROCESSING_SCORDITE'               , 5   ),
+	'spodumain'     => env('EVE_PROCESSING_SPODUMAIN'              , 5   ),
+	'veldspar'      => env('EVE_PROCESSING_VELDSPAR'               , 5   ),
+
+];
